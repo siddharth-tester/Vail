80253683d33697c2f9ec3715d9cc48d112c8abab
Siddharth <guptasiddharth@live.com>
2023-10-10 13:18:12 -0500

    second commit

--- VailAssignment.java
+++ VailAssignment.java
@@ -3,16 +3,24 @@ import java.util.Scanner;
 public class VailAssignment {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		/**
+		 * Reading the input from user for the position value.
+		 */
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter the position value = ");
 		int position = scan.nextInt();
-		int a[] = { 1, 2, 3, 4, 5, 6, 7 };
-		
-		
 
+		/**
+		 * This is the array to be rotated.
+		 */
+		int a[] = { 1, 2, 3, 4, 5, 6, 7 };
+
+		/**
+		 * Validating if the position value is negative.
+		 */
 		if (position < 0) {
-			System.out.println("Position should be positive.");
+			System.out.println("Position value should be positive.");
 		} else {
 			for (int i = 0; i < position; i++) {
 				int first = a[0];
@@ -22,6 +30,9 @@ public class VailAssignment {
 				}
 				a[j] = first;
 			}
+			/**
+			 * Printing the array after rotation.
+			 */
 			for (int j = 0; j < a.length; j++) {
 				System.out.print(a[j] + " ");
 			}
